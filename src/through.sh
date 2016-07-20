480b47e777cbbb4a1494024fefc86217310332ad
Martin Geisse <martingeisse@googlemail.com>
2016-07-20 20:53:55 +0200

    pin-through design

---
+++ src/through.sh
@@ -0,0 +1,2 @@
+export XST_SCRIPT_FILE=through.xst
+export CONSTRAINTS_FILE=through.ucf
---
+++ src/through.v
@@ -0,0 +1,8 @@
+
+module through(dataIn, ledOut);
+	input dataIn;
+	output ledOut;
+
+	assign ledOut = dataIn;
+
+endmodule
