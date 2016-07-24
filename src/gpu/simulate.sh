9165f6708e7d982cdc30978b97fce7e7fabbc4af
Martin Geisse <martingeisse@googlemail.com>
2016-07-24 13:26:45 +0200

    removed simulation script -- Icarus can't handle the verilog design anyway

--- src/gpu/simulate.sh
+++
@@ -1,2 +0,0 @@
-iverilog -o testbench.vvp -y ~/verilog-xilinx-primitives/unisims ~/verilog-xilinx-primitives/glbl.v display_hello.v ProgramMemory.v external/kcpsm3.v dsp/*.v testbench.v
-vvp testbench.vvp
