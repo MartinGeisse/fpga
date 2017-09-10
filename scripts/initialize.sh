a9d9cde95720fe64dc4f16bb06543d78d2ef97b8
Martin Geisse <martingeisse@googlemail.com>
2017-09-10 10:47:44 +0200

    scripts to load firmware

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -4,5 +4,6 @@ scp remote-scripts/build.txt martin@ise:auto-ise/build.sh
 scp remote-scripts/clean.txt martin@ise:auto-ise/clean.sh
 scp remote-scripts/upload.txt martin@ise:auto-ise/upload.sh
 scp remote-scripts/impact.txt martin@ise:auto-ise/impact.txt
+scp remote-scripts/load_firmware.txt martin@ise:auto-ise/load_firmware.sh
 ssh martin@ise chmod u+x 'auto-ise/*'
 ssh martin@ise ls -al auto-ise
